2ac145ad7730f0b92c1fd1510b8702052c609a53
Milly <milly.ca@gmail.com>
2024-05-23 19:30:15 +0900

    feat: export types in separate modules

--- examples/upper_case.ts
+++ examples/upper_case.ts
@@ -1,3 +1,14 @@
+/**
+ * Example module for [streamtest][]
+ *
+ * [streamtest]: https://jsr.io/@milly/streamtest
+ *
+ * @module
+ */
+
+/**
+ * Transform string chunk to upper case.
+ */
 export class UpperCase extends TransformStream<string, string> {
   constructor() {
     super({
--- mod.ts
+++ mod.ts
@@ -1,2 +1,13 @@
+/**
+ * Provides helper functions that make it easier to test [Streams API][]
+ * with various scenarios and assertions.
+ *
+ * [streamtest]: https://jsr.io/@milly/streamtest
+ * [Streams API]: https://developer.mozilla.org/docs/Web/API/Streams_API
+ *
+ * @module
+ */
+
 export * from "./errors/mod.ts";
 export * from "./test_stream.ts";
+export * from "./types.ts";
